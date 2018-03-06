5d38df0b23e54e6cf3c5afec248543614964f4fe
Arthur Philippe <arthur.philippe@epitech.eu>
2018-03-06 12:45:45 +0100

    basic entrypoint

---
+++ games/DefaultGame/Entry.cpp
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2018
+** cpp_arcade
+** File description:
+** Entry
+*/
+
+#include <iostream>
+
+extern "C" {
+	void Entry(void)
+	{
+		std::cout << "kappa, i'm the entry point" << std::endl;
+	}
+}
--- src/Main.cpp
+++ src/Main.cpp
@@ -6,8 +6,28 @@
 */
 
 #include <iostream>
+#include <dlfcn.h>
 
 int main()
 {
 	std::cout << "Hi :3" << std::endl;
+	void* handle = dlopen("./games/DefaultGame/libDefaultGame.so", RTLD_LAZY);
+
+	if (!handle) {
+		std::cerr << "Cannot open library: " << dlerror() << std::endl;
+		return 1;
+	}
+
+	typedef void (*entry_t)(void);
+	dlerror();
+	entry_t entry = (entry_t) dlsym(handle, "Entry");
+	const char *dlsym_error = dlerror();
+	if (dlsym_error) {
+		std::cerr << "Cannot load symbol 'hello': " << dlsym_error <<
+			'\n';
+		dlclose(handle);
+		return 1;
+	}
+	entry();
+	dlclose(handle);
 }
